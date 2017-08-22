f00c89d226ac93456c8ca2cfbd56a0decf93ddc8
Seqian Wang <seqian@users.noreply.github.com>
2017-08-22 00:19:10 -0400

    Added some structure
    Still early phase, just added a bunch of design and things to code

--- FitnessPal.py
+++ FitnessPal.py
@@ -1,14 +1,31 @@
-# myfitnesspal store-password seqian@outlook.com
+#!/usr/bin/python
+"""
+Created on Tue Aug 22 00:16:12 2017
+
+@author: Seqian Wang
+
+Purpose: Employs python-myfitnesspal to export meal information into a CSV file. Automatically detect last time run from CSV, then import relevant data.
+"""
+
+# myfitnesspal store-password username
+# Thanks for work @ https://github.com/coddingtonbear/python-myfitnesspal
 
 import myfitnesspal
+import datetime
 
+# Look into http://www.tutorialspoint.com/python/python_date_time.htm for properly using date and time in Python
+
+# Will be reading from a username.login file, which is essentially a text file with the relevant info
 client = myfitnesspal.Client('seqian@outlook.com')
 
-year = 2017
-month = 8
-day = 11
+firstDate = '2017-08-11'
+# Would come from csv file
+# lastBackup
+today = datetime.datetime.today()
 
 meals = client.get_date(2017, 8, 11)
 
 for i in range(day, 21myfmy):
 	meals = client.get_date(year, month, 2)
+    
+# Write to CSV file
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ExportMyFitnessPal
+
+Employs python-myfitnesspal from coddingtonbear to export MyFitnessPal data to CSV file
+Pretty straightforward, run once a week to export and append
+https://github.com/coddingtonbear/python-myfitnesspal
+
+On Windows, requires curses, for which binary files can be found at http://www.lfd.uci.edu/~gohlke/pythonlibs/#curses
+
+Thanks to https://stackoverflow.com/questions/35850362/importerror-no-module-named-curses-when-trying-to-import-blessings
+
+Note to future self:
+1. Remove email address from online repository
+2. Add to exclusion files email address, + csv files
