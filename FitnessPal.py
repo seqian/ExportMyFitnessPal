382c2ffeab41861b9c96327c5ac114dc823a5508
Seqian Wang <seqian@users.noreply.github.com>
2017-08-22 00:08:01 -0400

    Initial commit

---
+++ FitnessPal.py
@@ -0,0 +1,14 @@
+# myfitnesspal store-password seqian@outlook.com
+
+import myfitnesspal
+
+client = myfitnesspal.Client('seqian@outlook.com')
+
+year = 2017
+month = 8
+day = 11
+
+meals = client.get_date(2017, 8, 11)
+
+for i in range(day, 21myfmy):
+	meals = client.get_date(year, month, 2)
